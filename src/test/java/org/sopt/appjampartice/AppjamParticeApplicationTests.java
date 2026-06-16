7620e41cc36648b5f4c7a69d07b9d83a59ead4d3
guswnsj0112@naver.com <guswnsj0112@naver.com>
2026-06-16 21:44:10 +0900

    "chore: 프로젝트 초기 세팅"

---
+++ src/test/java/org/sopt/appjampartice/AppjamParticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.appjampartice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppjamParticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
